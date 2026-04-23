3d5e8ecf31b9c625a0f482c730b7a4f3f5491768
tushar jana <145481506+Tushar3002@users.noreply.github.com>
2026-04-23 19:32:46 +0530

    added teh Validations

---
+++ Practice2/middleware/validations.middleware.js
@@ -0,0 +1,13 @@
+export const validate=(schema)=>(req,res,next)=>{
+    try {
+        req.body=schema.parse(req.body)
+        console.log(req.body);
+        
+        next()
+    } catch (error) {
+      return res.status(400).json({
+        message:error.message
+      })
+        
+    }
+}
