df944f68dbb35a9dde21fd4f9172811b31201f0e
tushar jana <145481506+Tushar3002@users.noreply.github.com>
2026-04-30 15:04:06 +0530

    added payment status update from in admin

--- Practice2/controllers/admin.controller.js
+++ Practice2/controllers/admin.controller.js
@@ -74,6 +74,22 @@ export const updateOrderStatus = async (req, res) => {
   }
 };
 
+export const updatePaymentStatus = async(req,res)=>{
+  try {
+    const {id}= req.params;
+    const {paymentStatus}=req.body;
+    const order=await Order.findByPk(id)
+    if(!order) return res.json(404).json({message:"Order Not Found"})
+
+    order.paymentStatus=paymentStatus
+    await order.save()
+    res.status(200).json({message:"Status Updated successfully"})
+
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+}
+
 
 export const getAllUsersDetails = async(req,res)=>{
   try {
--- Practice2/routes/order.route.js
+++ Practice2/routes/order.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createOrder, getOrder } from "../controllers/order.controller.js";
 import { isAdmin, verifyToken } from "../middleware/auth.middleware.js";
-import { getAllOrders, updateOrderStatus } from "../controllers/admin.controller.js";
+import { getAllOrders, updateOrderStatus, updatePaymentStatus } from "../controllers/admin.controller.js";
 
 const OrderRouter = Router();
 
@@ -12,4 +12,5 @@ OrderRouter.get("/",verifyToken,getOrder)
 
 OrderRouter.get("/admin", verifyToken, isAdmin, getAllOrders);
 OrderRouter.put("/:id/status", verifyToken, isAdmin, updateOrderStatus);
+OrderRouter.put("/:id/paymentStatus", verifyToken, isAdmin, updatePaymentStatus);
 export default OrderRouter;
